8317850ecb39a574eeab7f368a1c96c75b6e6899
PedroAvila <minimainor_austin@hotmail.com>
2021-05-10 14:31:15 -0500

    Creating Custom Console Theme for SeriLog Logging

--- CMS_CORE_NG/Program.cs
+++ CMS_CORE_NG/Program.cs
@@ -1,11 +1,12 @@
+using LoggingService;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace CMS_CORE_NG
 {
@@ -13,13 +14,41 @@ namespace CMS_CORE_NG
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    int zero = 0;
+                    int result = 100 / zero;
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Log.Error("An error occurrend while seeding the database {Error} {StackTrace} {InnerException} {Source}", ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSerilog((hostingContext, loggingConfiguration) => loggingConfiguration
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("Application", "CMS_APP")
+                    .Enrich.WithProperty("MachineName", Environment.MachineName)
+                    .Enrich.WithProperty("CurrentManagedThreadId", Environment.CurrentManagedThreadId)
+                    .Enrich.WithProperty("OSVersion", Environment.OSVersion)
+                    .Enrich.WithProperty("Version", Environment.Version)
+                    .Enrich.WithProperty("UserName", Environment.UserName)
+                    .Enrich.WithProperty("ProcessId", Process.GetCurrentProcess().Id)
+                    .Enrich.WithProperty("ProcessName", Process.GetCurrentProcess().ProcessName)
+                    .WriteTo.Console(theme: AnsiConsoleTheme.None)
+                    .WriteTo.File(formatter: new CustomTextFormatter(), path: Path.Combine(hostingContext.HostingEnvironment.ContentRootPath + $"{Path.DirectorySeparatorChar}Logs{Path.DirectorySeparatorChar}", $"load_error_{DateTime.Now:yyyyMMdd}.txt"))
+                    .ReadFrom.Configuration(hostingContext.Configuration)); ;
                     webBuilder.UseStartup<Startup>();
                 });
     }

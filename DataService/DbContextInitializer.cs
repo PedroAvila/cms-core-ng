e713195c86fcaa565708e3e8292c57c698ae55f4
PedroAvila <minimainor_austin@hotmail.com>
2021-05-11 07:32:21 -0500

    Creating Database with Entity Framework Core- Part 2

--- DataService/DbContextInitializer.cs
+++ DataService/DbContextInitializer.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using FuntionalService;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataService
 {
-    class DbContextInitializer
+    public static class DbContextInitializer
     {
+        public static async Task Initialize(DataProtectionKeysContext dataProtectionKeysContext, ApplicationDbContext applicationDbContext, IFunctionalSvc functionalSvc)
+        {
+            // Check, if db DataProtectionKeysContext is crated
+            // Check, if db ApplicationDbContext is created
+            await dataProtectionKeysContext.Database.EnsureCreatedAsync();
+            await applicationDbContext.Database.EnsureCreatedAsync();
+
+            // Check, if db contains any users. If db is not empty, then db has been already seeded
+            if (applicationDbContext.ApplicationUsers.Any())
+            {
+                return;
+            }
+
+            // if empty create Admin User and App User
+            await functionalSvc.CreateDefaultAdminUser();
+            await functionalSvc.CreateDefaultUser();
+        }
     }
 }
---
+++ FuntionalService/IFunctionalSvc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuntionalService
+{
+    public interface IFunctionalSvc
+    {
+        Task CreateDefaultAdminUser();
+        Task CreateDefaultUser();
+    }
+}
